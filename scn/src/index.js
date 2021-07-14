4c474d34b55d6da93c7ffc7867b183cec96de17f
CooperR1 <cooperreed7@gmail.com>
2021-07-13 23:21:43 -0400

    Summer 2021 Updates
    -Most html changes took place in defaults.html, full frontpage restructure test
    -Fonts imported (Bebas Neue)
    -Widgets still not connected to anything (do we have a calendar?), placeholders for now
    -Some CSS rearrangement to be done
    -side pages still waiting on design influence (Team, Gallery, events if we want it, otherwise could merge events and gallery)
    -Video no longer to be used (Frost limitation), so will swap out soon for Paolo's sick reel

--- scn/src/index.js
+++ scn/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import FontFace from './assets/fonts/BebasNeue-Regular.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
